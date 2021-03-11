b13171923ce07f6b32ea4709258864d173099d56
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-03-11 12:48:47 +0700

    First Time in Node Express

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,35 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+app.get("/api/timestamp/:date?", (req, res) => {
+  const entry = req.params.date
+  let utc, unix
+  if (entry) {
+    const regex = /[^0-9]/
+    if (regex.test(entry) === true) {
+      if (new Date(entry) != 'Invalid Date') {
+        utc = new Date(entry).toUTCString()
+        unix = new Date(entry).getTime()
+      }
+      else {
+        res.json({ error: "Invalid Date" })
+      }
+
+    }
+    else {
+      utc = new Date(parseInt(entry)).toUTCString()
+      unix = new Date(parseInt(entry)).getTime()
+    }
+  }
+  else {
+    utc = new Date().toUTCString()
+    unix = new Date().getTime()
+  }
+  res.json({ unix: unix, utc: utc })
+})
 
 
 
